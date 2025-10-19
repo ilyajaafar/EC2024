f5745cf0abe3244c73ff2cd6b2c023bf891eacd9
ilyajaafar <s22a0036@siswa.umk.edu.my>
2025-10-19 14:48:34 +0800

    Update tutorial1.py

--- tutorial1.py
+++ tutorial1.py
@@ -13,6 +13,13 @@ df = pd.DataFrame(data)
 
 st.title("📊 Data Visualization Dashboard")
 
+col1, col2, col3, col4 = st.columns(4)
+    
+col1.metric(label="PLO 2", value=f"3.3", help="PLO 2: Cognitive Skill", border=True)
+col2.metric(label="PLO 3", value=f"3.5", help="PLO 3: Digital Skill", border=True)
+col3.metric(label="PLO 4", value=f"4.0", help="PLO 4: Interpersonal Skill", border=True)
+col4.metric(label="PLO 5", value=f"4.3", help="PLO 5: Communication Skill", border=True)
+
 # ================================
 # 1️⃣ Gender distribution (Arts Faculty)
 # ================================
